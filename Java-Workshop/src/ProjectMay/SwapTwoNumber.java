3b7ac0d1c1e28e8ae95d2c64b5f9d24276112219
vipul <vipulsajjanwar@gmail.com>
2020-05-05 11:50:36 +0530

    Second commit
    uploading basic numbers swapping program

---
+++ Java-Workshop/src/ProjectMay/SwapTwoNumber.java
@@ -0,0 +1,13 @@
+package ProjectMay;
+
+public class SwapTwoNumber {
+public static void main(String[] args) {
+	int n1=10;
+	int n2=20;
+	
+	int temp=n1;
+	n1=n2;
+	n2=temp;
+	System.out.println("Swap: "+n1+" "+n2);
+}
+}
---
+++ Java-Workshop/src/ProjectMay/SwapWithoutUSingThirdVariable.java
@@ -0,0 +1,15 @@
+package ProjectMay;
+
+public class SwapWithoutUSingThirdVariable {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("swap: "+a+ " "+b);
+	}
+
+}
